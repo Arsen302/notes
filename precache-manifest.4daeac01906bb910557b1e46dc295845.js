610bd35d6d61cc85e55dd9ebe218fad0dedcf9fa
Arsen302 <aarakelyan98@gmail.com>
2020-07-30 17:11:51 +0300

    first commit

---
+++ precache-manifest.4daeac01906bb910557b1e46dc295845.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1db4be90d4c969aac73d714c6db18d6",
+    "url": "/notes/index.html"
+  },
+  {
+    "revision": "4f184bdea9b54fb1424e",
+    "url": "/notes/static/css/main.fd729460.chunk.css"
+  },
+  {
+    "revision": "133d61bc89558bdef40f",
+    "url": "/notes/static/js/2.b9491924.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/notes/static/js/2.b9491924.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f184bdea9b54fb1424e",
+    "url": "/notes/static/js/main.b4b77e00.chunk.js"
+  },
+  {
+    "revision": "b4e7f333cec4847613e3",
+    "url": "/notes/static/js/runtime-main.9f0c36eb.js"
+  }
+]);
